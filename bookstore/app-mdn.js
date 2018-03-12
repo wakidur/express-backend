70e0010f2cd180d15a88f50b10c5f22ef020fd76
wakidur <wakidr@gmail.com>
2018-03-12 20:52:13 +0600

    #dd

---
+++ bookstore/app-mdn.js
@@ -0,0 +1,11 @@
+
+var express = require('express');
+var app = express();
+
+app.get('/', (req, res) => {
+	res.send("hello Wrold!");
+});
+
+app.listen(3000, () => {
+	console.log("Example app listening on port 3000!" );
+});
