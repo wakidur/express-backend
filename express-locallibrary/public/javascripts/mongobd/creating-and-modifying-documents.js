c689317e86f24760fdbfa73493059db278eb8e78
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-03-27 11:13:41 +0600

    #add

---
+++ express-locallibrary/public/javascripts/mongobd/creating-and-modifying-documents.js
@@ -0,0 +1,11 @@
+// step -1 
+
+/**
+ * Create an instance of model someModel
+ * 
+ */
+
+ var awesom_instance = new SomeModel({name: 'awesome'});
+ /**
+  * Save the new model instance, passsing a callback
+  */
