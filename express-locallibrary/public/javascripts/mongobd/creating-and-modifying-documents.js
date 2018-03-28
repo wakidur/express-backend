a4aad0fd13c77417d31805bf11005c120b70fa02
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-03-28 11:09:46 +0600

    #study

--- express-locallibrary/public/javascripts/mongobd/creating-and-modifying-documents.js
+++ express-locallibrary/public/javascripts/mongobd/creating-and-modifying-documents.js
@@ -1,14 +1,59 @@
 // step -1 
 
 /**
- * Create an instance of model someModel
+ * create a record 
  * 
  */
 
+ // Create an instance of model SomeModel
  var awesom_instance = new SomeModel({name: 'awesome'});
+ 
+ // Save the new model instance, passsing a callback
+ awesom_instance.save( function(err){
+   if( err ) 
+    return handleError(err);
+    //save
+ });
 
  /**
-  * Save the new model instance, passsing a callback
+  * use create() to define the model instance at the same time as you save it.
   */
+  SomeModel.create({name: 'also_awesome'}, function(err, awesome_instance) {
+     if(err) 
+      return handleError(err);
+      // saved!
+  });
+
+  /**
+   * 
+   */
+  // Access model field value using dot notation 
+  console.log(awesom_instance.name); 
+  // change recode by modifying the fields, thend calling 
+
+  awesom_instance.name = "New cool name";
+  awesom_instance.save(function(err){
+    if (err) {
+      return handleError(err); // saved!
+    }
+  });
+
+
+  /**
+   * Searching for records 
+   * 
+   */
+
+   var Athlete = mongoose.model('Athlete', yourSchema);
+   
+   // Find all athletes who play tennis, selectiong the "name" and "age" fieldes
+  Athlete.find({'sport': 'Tennis'}, 'name age', function(err, athletes){
+    if(err) {
+      return handleError(err);
+    }
+      // 'athletes' contains the list of athletes that match the criteria.
+  });
+
+  
 
   
