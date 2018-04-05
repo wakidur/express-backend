f142c01ef500477c8c5c8df1b2d945c9485ee71d
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-04-05 17:33:21 +0600

    #add new

--- express-locallibrary/controllers/genreController.js
+++ express-locallibrary/controllers/genreController.js
@@ -1,5 +1,13 @@
+/*import  genreModel*/
 var Genre = require('../models/genreModel');
+/* import  bookModel */
 var Book = require('../models/bookModel');
+/* Import validation and sanitisation methods */
+const { body, validationResult } = require('express-validator/check');
+const { sanitizeBody  } = require('express-validator/filter');
+
+
+/* Import async */
 var async = require('async');
 /**
 * Display list of all Genre.
@@ -69,14 +77,76 @@ exports.genre_detail = function (req, res, next) {
 
 
 // Display Genre create form on GET.
+/*
 exports.genre_create_get = function (req, res) {
 	res.send('NOT IMPLEMENTED: Genre create GET');
 };
+*/
+exports.genre_create_get = function (req, res, next) {
+	res.render('./genre/genreFormView', { title: 'Create Genre'});
+};
 
 // Handle Genre create on POST.
+/*
 exports.genre_create_post = function (req, res) {
 	res.send('NOT IMPLEMENTED: Genre create POST');
 };
+*/
+
+exports.genre_create_post = [
+	// validate that the name field is not empty
+	body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+
+	// Sanitize  ( trim and escape ) the name field
+	sanitizeBody('name').trim().escape(),
+
+	// Process request after validation  and sanitization
+
+	(req, res, next) => {
+		// Extranct the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create a genre object with escaped and trimmed data.
+		var genre = new Genre(
+			{
+				name: req.bodey.name
+			}
+		);
+
+		if (!errors.isEmpty() ) {
+			// There are errors. Render the form again with sanitize values/error message.
+			res.render('./genre/genreFormView', {
+				title: "Create Genre", 
+				genre: genre,
+				errors: errors.array()
+			});
+			return;
+			
+		} else {
+			// data from form is valid.
+			// check if genre with same name already exists.
+			Genre.findOne({ 'name': req.body.name })
+				.exec((err, found_genre) => {
+					if (err) {
+						return next(err);
+					}
+					if( found_genre ){
+						// Genre exists, redirect to its detail page.
+						res.redirect( found_genre.url );
+					} else {
+						genre.save((err) => {
+							if (err) {
+								return next(err);
+							}
+							// Genre saved. Redirect to genre detail page.
+							res.redirect(genre.url);
+						});
+					}
+				});
+		}
+	}
+
+];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function (req, res) {
