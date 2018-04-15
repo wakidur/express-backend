b0be0597a5e39ff193e358a1b11b538be43204f1
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-04-15 17:32:31 +0600

    #add value

--- express-locallibrary/app.js
+++ express-locallibrary/app.js
@@ -18,7 +18,10 @@ mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
+db.once('open', function() {
+  // we're connected!
+  console.log("we're connected");
+});
 
 // specifying an URL path of 'wiki'
 // var wiki = require('./routes/route/wiki');
