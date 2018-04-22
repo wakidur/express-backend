d5c4a1a7eabac2326e9759ffdcefebb6968cc2d1
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-04-22 15:35:43 +0600

    #add

---
+++ express-locallibrary/controllers/authorController-test.js
@@ -0,0 +1,59 @@
+var Author = require('../models/authorModel');
+
+// function declaration 
+exports.authorList  = authorList;
+exports.authorDetail  = authorDetail ;
+exports.authorCreateGet  = authorCreateGet ;
+exports.authorCreatePost  = authorCreatePost  ;
+exports.authorDeleteGet  = authorDeleteGet   ;
+exports.authorDeletePost  = authorDeletePost   ;
+exports.authorUpdateGet  = authorUpdateGet   ;
+exports.authorUpdatePost  = authorUpdatePost   ;
+
+
+// Display list of all Authors.
+function authorList(req, res) {
+    res.send('Not Implemented: Author list');
+    
+}
+
+// Display detail page for a specific Author.
+function authorDetail(req, res) {
+    res.send('Not Implemented: Author list');
+    
+}
+
+// Display Author create form on GET.
+function authorCreateGet(req, res) {
+    res.send('Not Implemented: Author list');
+    
+}
+
+// Handle Author create on POST.
+function authorCreatePost(req, res) {
+    res.send('Not Implemented: Author list');
+    
+}
+
+// Display Author delete form on GET.
+function authorDeleteGet(req, res) {
+    res.send('Not Implemented: Author list');
+    
+}
+
+// Handle Author delete on POST.
+function authorDeletePost(req, res) {
+    res.send('Not Implemented: Author list');
+    
+}
+
+// Display Author update form on GET.
+function authorUpdateGet(req, res) {
+    res.send('Not Implemented: Author list');
+    
+}
+
+// Handle Author update on POST.
+function authorUpdatePost(req, res) {
+    res.send('Not Implemented: Author list');
+}
