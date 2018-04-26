1371df05777e21e6a427c15a4192dcf236222fb9
NEXT_IT\Wakidur <wakidur@gmail.com>
2018-04-26 16:44:43 +0600

    #add new file

---
+++ simple-knowledgebase-app/models/articles.js
@@ -0,0 +1,21 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let articleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model('articles', articleSchema);
+
