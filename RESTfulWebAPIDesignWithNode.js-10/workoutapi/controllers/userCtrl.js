8f57eb3630623b231f4cda40ca4ae7600bff9111
NEXT_IT\Wakidur <wakidur@gmail.com>
2019-02-25 18:04:34 +0600

    #sdfd

--- RESTfulWebAPIDesignWithNode.js-10/workoutapi/controllers/userCtrl.js
+++ RESTfulWebAPIDesignWithNode.js-10/workoutapi/controllers/userCtrl.js
@@ -2,35 +2,41 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const _ = require('lodash');
 const Joi = require('joi');
+const path = require('path');
 const multer = require('multer');
 const User = require('../model/userSchema');
 
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, 'uploads/');
+    destination: function (req, file, next) {
+        next(null, path.join(__dirname, '../uploads/'));
     },
-    filename: function(req, file, cb) {
-      cb(null, new Date().toISOString() + file.originalname);
+    filename: function (req, file, next) {
+        const now = new Date().toISOString();
+        const date = now.replace(/:/g, '-');
+        next(null, date + file.originalname);
     }
-  });
-  
-  const fileFilter = (req, file, cb) => {
-    // reject a file
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
-    } else {
-      cb(null, false);
-    }
-  };
-  
-  const upload = multer({
+});
+const upload = multer({
     storage: storage,
-    limits: {
-      fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
-  }).single('userImage');
+}).single('userImage');
+
+//   const fileFilter = (req, file, cb) => {
+//     // reject a file
+//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+//       cb(null, true);
+//     } else {
+//       cb(null, false);
+//     }
+//   };
+
+//   const upload = multer({
+//     storage: storage,
+//     limits: {
+//       fileSize: 1024 * 1024 * 5
+//     },
+//     fileFilter: fileFilter
+//   }).single('userImage');
 
 /**
  * GET /signup
@@ -50,121 +56,88 @@ exports.getSignup = (req, res) => {
  * Create a new local account.
  */
 exports.postSignup = (req, res, next) => {
-    upload(req, res, function (err) {
-        
-        if (err instanceof multer.MulterError) {
-          // A Multer error occurred when uploading.
-          console.log(err);
-          res.status(201).json(err);
-        } else if (err) {
-          // An unknown error occurred when uploading.
-          res.status(201).json(err);
-        }
-        const user = new User({
-            _id: new mongoose.Types.ObjectId(),
-            fullname: req.body.fullname,
-            email: req.body.email,
-            password: req.body.password,
-            userImage: req.file.path 
-          });
-        user.save().then((result) => {
-            res.status(200).json(result);
-        }).catch((err) => {
-            if (err.name === 'ValidationError') {
-                var valErrors = [];
-                Object.keys(err.errors).forEach((key) => {
-                    var b = {};
-                    b[key] = err.errors[key].message;
-                    valErrors.push(b);
+
+    try {
+        upload(req, res, function (err) {
+            if (err instanceof multer.MulterError) {
+                // A Multer error occurred when uploading.
+                console.log(err);
+                res.status(201).json({
+                    message: "A Multer error occurred when uploading.",
+                    error: err
                 });
-                return res.status(404).send(valErrors);
+            } else if (err) {
+                // An unknown error occurred when uploading.
+                res.status(201).json({
+                    message: "An unknown error occurred when uploading.",
+                    error: err
+                });
+            } else {
+                if (!req.body) {
+                    res.status(400).json({
+                        "message": "User content can not be empty"
+                    });
+                    return;
+                }
+                // Create a Exercise
+                const userSchema = Joi.object({
+                    fullname: Joi.string().required(),
+                    email: Joi.string().email(),
+                    password: Joi.string().required(),
+                });
+                Joi.validate(req.body, userSchema, (err, value) => {
+                    if (err) {
+                        next(err);
+                    } else {
+                        User.findOne({
+                            email: value.email
+                        }).then((result) => {
+                            if (result) {
+                                return res.status(200).json({
+                                    message: 'Account with that email address already exists.'
+                                });
+                            } else {
+                                if (req.file) {
+                                    var user = new User({
+                                        fullname: value.fullname,
+                                        email: value.email,
+                                        password: value.password,
+                                        userImage: req.file.filename,
+                                    });
+                                } else {
+                                    var user = new User({
+                                        fullname: value.fullname,
+                                        email: value.email,
+                                        password: value.password,
+                                    });
+                                }
+                                user.save().then((result) => {
+                                    res.status(200).json(result);
+                                }).catch((err) => {
+                                    if (err.name === 'ValidationError') {
+                                        var valErrors = [];
+                                        Object.keys(err.errors).forEach((key) => {
+                                            var b = {};
+                                            b[key] = err.errors[key].message;
+                                            valErrors.push(b);
+                                        });
+                                        return res.status(404).send(valErrors);
+                                    }
+                                    return res.status(404).json(err);
+                                });
+                            }
+                        }).catch((err) => {
+                            return res.status(404).json(err);
+                        });
+                    }
+                });
             }
-            return res.status(404).json(err);
         });
-      });
-
-
-
-
-
-
-
-
-
-
-
-    // try {
-    //     if (!req.body) {
-    //         res.status(400).json({
-    //             "message": "User content can not be empty"
-    //         });
-    //         return;
-    //     }
-    //     // Create a Exercise
-    //     const userSchema = Joi.object({
-    //         fullname: Joi.string().required(),
-    //         email: Joi.string().email(),
-    //         password: Joi.string().required(),
-    //         // userImage: Joi.string().required(),
-    //     });
-    //     Joi.validate(req.body, userSchema, (err, value) => {
-    //         if (err) {
-    //             next(err);
-    //         } else {
-    //             User.findOne({
-    //                 email: value.email
-    //             }).then((result) => {
-    //                 if (result) {
-    //                     return res.status(200).json({
-    //                         message: 'Account with that email address already exists.'
-    //                     });
-    //                 } else {
-    //                     upload(req, res, function (err) {
-    //                         if (err instanceof multer.MulterError) {
-    //                           // A Multer error occurred when uploading.
-    //                         } else if (err) {
-    //                           // An unknown error occurred when uploading.
-    //                         }
-                         
-    //                         const userObject = {
-    //                             fullname: value.fullname,
-    //                             email: value.email,
-    //                             password: value.passport,
-    //                             userImage: req.file.path,
-    //                         }
-    //                         let user = new User(userObject);
-    //                         user.save().then((result) => {
-    //                             res.status(200).json(result);
-    //                         }).catch((err) => {
-    //                             if (err.name === 'ValidationError') {
-    //                                 var valErrors = [];
-    //                                 Object.keys(err.errors).forEach((key) => {
-    //                                     var b = {};
-    //                                     b[key] = err.errors[key].message;
-    //                                     valErrors.push(b);
-    //                                 });
-    //                                 return res.status(404).send(valErrors);
-    //                             }
-    //                             return res.status(404).json(err);
-    //                         });
-    //                       });
-
-                        
-
-
-
-
-    //                 }
-    //             }).catch((err) => {
-    //                 res.status(404).json(err);
-    //             });
-    //         }
-    //     });
-    // } catch (error) {
-    //     res.json({
-    //         "message": "block of code for errors!"
-    //     });
-    // }
+    } catch (error) {
+        res.json({
+            "message": "block of code for errors!"
+        });
+    }
 };
 
 
@@ -231,7 +204,7 @@ exports.postLogin = (req, res, next) => {
  * GET /account
  * get All Account.
  */
-exports.getAllAccount = (req, res) => { };
+exports.getAllAccount = (req, res) => {};
 
 
 
@@ -242,12 +215,20 @@ exports.getAllAccount = (req, res) => { };
 
 
 exports.getUserProfile = (req, res, next) => {
-    User.findOne({ _id: req._id },
+    User.findOne({
+            _id: req._id
+        },
         (err, user) => {
             if (!user)
-                return res.status(404).json({ status: false, message: 'User record not found.' });
+                return res.status(404).json({
+                    status: false,
+                    message: 'User record not found.'
+                });
             else
-                return res.status(200).json({ status: true, user : _.pick(user,['fullname','email']) });
+                return res.status(200).json({
+                    status: true,
+                    user: _.pick(user, ['fullname', 'email'])
+                });
         }
     );
 };
--- RESTfulWebAPIDesignWithNode.js-10/workoutapi/routes/users.js
+++ RESTfulWebAPIDesignWithNode.js-10/workoutapi/routes/users.js
@@ -1,53 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const path = require('path');
 const userCtrl = require('../controllers/userCtrl');
 const jwtHelper = require('../config/jwtHelper');
-const Joi = require('joi');
-const multer = require('multer');
-const User = require('../model/userSchema');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, next) {
-    next(null, path.join(__dirname, '../uploads/'));
-  },
-  filename: function (req, file, next) {
-    const now = new Date().toISOString();
-    const date = now.replace(/:/g, '-');
-    next(null, date + file.originalname);
-  }
-});
-
-
-
-// var storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, 'uploads');
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, file.fieldname + '-' + Date.now());
-//   }
-// })
-
-// var upload = multer({ storage: storage });
-
-// const fileFilter = (req, file, cb) => {
-//   // reject a file
-//   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-//     cb(null, true);
-//   } else {
-//     cb(null, false);
-//   }
-// };
-
-const upload = multer({
-  storage: storage,
-});
-
-
-
-
 
 /**
  * GET /signup
@@ -59,8 +13,8 @@ const upload = multer({
 // post request for creating a user.
 router
   .route('/signup')
-  .get(userCtrl.getSignup);
-// .post(userCtrl.postSignup);
+  .get(userCtrl.getSignup)
+  .post(userCtrl.postSignup);
 
 
 // post request for creating a authenticate.
@@ -78,74 +32,5 @@ router
   .route('/account/profile')
   .get(jwtHelper.verifyJwtToken, userCtrl.getUserProfile)
   .post(jwtHelper.verifyJwtToken, userCtrl.postUserUpdateProfile);
-
-
-router.route("/signup").post(
-  upload.single('userImage'), (req, res, next) => {
-    try {
-      if (!req.body) {
-        res.status(400).json({
-          "message": "User content can not be empty"
-        });
-        return;
-      }
-      // Create a Exercise
-      const userSchema = Joi.object({
-        fullname: Joi.string().required(),
-        email: Joi.string().email(),
-        password: Joi.string().required(),
-      });
-      Joi.validate(req.body, userSchema, (err, value) => {
-        if (err) {
-          next(err);
-        } else {
-          User.findOne({
-            email: value.email
-          }).then((result) => {
-            if (result) {
-              return res.status(200).json({
-                message: 'Account with that email address already exists.'
-              });
-            } else {
-              if (req.file) {
-                var user = new User({
-                  fullname: value.fullname,
-                  email: value.email,
-                  password: value.password,
-                  userImage: req.file.filename,
-                });
-              } else {
-                var user = new User({
-                  fullname: value.fullname,
-                  email: value.email,
-                  password: value.password,
-                });
-              }
-              user.save().then((result) => {
-                res.status(200).json(result);
-              }).catch((err) => {
-                if (err.name === 'ValidationError') {
-                  var valErrors = [];
-                  Object.keys(err.errors).forEach((key) => {
-                    var b = {};
-                    b[key] = err.errors[key].message;
-                    valErrors.push(b);
-                  });
-                  return res.status(404).send(valErrors);
-                }
-                return res.status(404).json(err);
-              });
-            }
-          }).catch((err) => {
-            return res.status(404).json(err);
-          });
-        }
-      });
-    } catch (error) {
-      res.json({
-        "message": "block of code for errors!"
-      });
-    }
-  });
 
 module.exports = router;
